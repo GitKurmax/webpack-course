8ee8104a64364426fd6a68193fa901023a33e8f3
Alexandr Kurmoiarov <alexandrmail32@gmail.com>
2020-05-26 00:19:22 +0300

    Iniial commit

---
+++ src/analytics.js
@@ -0,0 +1,40 @@
+function createAnalytics() {
+    let counter = 0;
+    let isDestroyed = false;
+    const clicksCollector = [];
+
+    const listener = () => {
+        const singleClick = {
+            clickNumber: counter++,
+            clickTime: Date.now()
+        }
+
+        clicksCollector.push(singleClick);
+    };
+    
+    document.addEventListener('click', listener);
+    
+    return {
+        destroy() {
+            document.removeEventListener('click', listener);
+            isDestroyed = true;
+        },
+
+        getClicks() {
+            if (isDestroyed) {
+                return 'Analytics is destroyed';
+            }
+            return counter;
+        },
+
+        getClicksCollection() {
+            if (isDestroyed) {
+                return 'Analytics is destroyed';
+            }
+
+            return clicksCollector;
+        }
+    }
+}
+
+window.analytics = createAnalytics();
